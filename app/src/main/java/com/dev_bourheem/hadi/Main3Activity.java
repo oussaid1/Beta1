a756df531eb4a1280964bde34070991e5a5fa733
oussaid1 <Tfds@6891g>
2019-12-24 17:56:31 +0000

    milestone 14.0

--- app/src/main/java/com/dev_bourheem/hadi/Main3Activity.java
+++ app/src/main/java/com/dev_bourheem/hadi/Main3Activity.java
@@ -2,13 +2,31 @@ package com.dev_bourheem.hadi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
-
+Button loginBtn;
+String pass="tigmi";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        loginBtn =findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if (pass = "")
+                OpentAvtivity3();
+            }
+        });
+    }
+    public void OpentAvtivity3() {
+        final Intent intent2;
+        intent2 = new Intent(this, MainActivity.class);
+        //intent1.putExtra("tarikh" ,date);
+        startActivity(intent2);
     }
 }
