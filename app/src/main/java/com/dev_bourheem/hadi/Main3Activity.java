c9b76c966fb46354b4b70203a7b1632690834a47
oussaid1 <Tfds@6891g>
2019-12-24 23:14:02 +0000

    milestone 15.0

--- app/src/main/java/com/dev_bourheem/hadi/Main3Activity.java
+++ app/src/main/java/com/dev_bourheem/hadi/Main3Activity.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
 Button loginBtn;
-String pass="tigmi";
+EditText username,PasswordIn,register;
+String userNm,PassIn;
+String pass="tigmi",usr="ana";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        PasswordIn=findViewById(R.id.PasswordIn);
+        username=findViewById(R.id.loginNmIn);
         loginBtn =findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //if (pass = "")
-                OpentAvtivity3();
+                logIn();
             }
         });
     }
@@ -28,5 +34,18 @@ String pass="tigmi";
         intent2 = new Intent(this, MainActivity.class);
         //intent1.putExtra("tarikh" ,date);
         startActivity(intent2);
+    }
+    public void logIn(){
+        userNm = username.getText().toString();
+        PassIn=PasswordIn.getText().toString();
+        if (userNm.equals(usr) && PassIn.equals(pass) )
+
+            OpentAvtivity3();
+
+         else    MsgBox("pass not correct");
+
+    }
+    public void MsgBox(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
