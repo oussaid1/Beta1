e6023db9f5a7b53da30758f52efb1ca4d9d7d109
oussaid1 <Tfds@6891g>
2020-01-04 15:46:12 +0000

    Version 1.3
    Added Quantity , AutoComplete Editext

---
+++ app/src/main/java/com/dev_bourheem/hadi/Main4Activity.java
@@ -0,0 +1,14 @@
+package com.dev_bourheem.hadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
